2bf481cb91e49c3216d0512b1f2438d4cb908090
Paulo Marinato <marinatopaulo@gmail.com>
2023-05-05 23:54:35 -0300

    configurando função localStorage

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,17 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
+const itens = []
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
+
+    criaElemento(nome.value, quantidade.value)
+
+    nome.value = ""
+    quantidade.value = ""
 })
 
 function criaElemento(nome, quantidade) {
@@ -13,9 +20,18 @@ function criaElemento(nome, quantidade) {
 
     const numeroItem = document.createElement('strong')
     numeroItem.innerHTML = quantidade
-
     novoItem.appendChild(numeroItem)
+
     novoItem.innerHTML += nome
 
     lista.appendChild(novoItem)
+
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    }
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("item", JSON.stringify(itens))
 }
