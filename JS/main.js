fd0a485c57a5b47b0c6027a7b6ef33ad77ac5b51
Paulo Marinato <marinatopaulo@gmail.com>
2023-05-05 23:28:39 -0300

    acondionando variáveis.

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,21 @@
-const form = document.getElementById("novoItem");
+const form = document.getElementById("novoItem")
+const lista = document.getElementById("lista")
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
-    console.log(evento)
-    console.log(evento.target.elements['nome'].value)
-    console.log(evento.target.elements['quantidade'].value)
+    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+})
 
-});
+function criaElemento(nome, quantidade) {
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += nome
+
+    lista.appendChild(novoItem)
+}
